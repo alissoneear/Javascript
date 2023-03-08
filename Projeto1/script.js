465e447432f86cd43fff4a8d8b7054035c392ba0
Alisson Lourenço <alissoneear@gmail.com>
2023-03-07 21:27:12 -0300

    Test

--- Projeto1/script.js
+++ Projeto1/script.js
@@ -1,4 +1,5 @@
-function entrar(){
+
+function entrar() {
     var area = document.getElementById('area');
     var text = prompt('Digite o seu nome:');
 
@@ -9,3 +10,31 @@ function entrar(){
         area.innerHTML = 'Bem vindo ' + text;
     }
 }
+
+//sessionStorage --é excluído quando fecha o navegador.
+//localStorage --cookies que fica no navegador mesmo fechando o mesmo.
+localStorage.clear();
+if (!typeof localStorage.nomediv == "undefined") {
+    localStorage.nomediv = prompt("Digite seu nome: ");
+}
+nomediv = localStorage.nomediv;
+document.getElementById('nome').innerHTML = nomediv;
+
+
+//Spread Operator
+function cadastroPessoa(info) {
+    let novosDados = {
+        ...info,
+        cargo: "Desenvolvedor",
+        código: 45114795
+    };
+    return novosDados;
+}
+console.log(cadastroPessoa({ nome: "Alisson", sobrenome: "Silva", idade: 25 }));
+
+
+//Rest Operator
+function minhalista(...nomes) {
+    console.log(nomes);
+}
+minhalista("Alisson", "Lucas", "Kevin");

5aba98c956ac986ddfba95757dc95b15cd7b4733
Alisson Lourenço <alissoneear@gmail.com>
2023-03-09 17:37:33 -0300

    Finished

--- Projeto1/script.js
+++ Projeto1/script.js
@@ -38,3 +38,41 @@ function minhalista(...nomes) {
     console.log(nomes);
 }
 minhalista("Alisson", "Lucas", "Kevin");
+
+
+
+//map percorre todos os itens de uma lista e retornar algo que a gente queira
+const lista = [1, 2, 3, 4, 5, 6];
+const listaMulti = lista.map(function (item) { return item * 5 });
+console.log(listaMulti);
+
+//map com index(posição do número no array)
+const listaSumIndex = lista.map(function (item, index) { return item + index });
+console.log(listaSumIndex);
+
+//reduce -> pega o total que começa com 0 e soma com o próximo número e assim por diante, e retorna um número só.
+const listaReduce = lista.reduce(function (total, proximo) { return total + proximo });
+console.log(listaReduce);
+
+//find -> ele retorna "undefined" caso não encontre o item na lista.
+const listaFind = lista.find(function (item) { return item === 6; });
+console.log(listaFind);
+
+
+//função no modo normal:
+function adicionar(...numeros) {
+    let total = numeros.reduce(function (total, proximo) { return total + proximo });
+    console.log(total);
+};
+adicionar(1, 2, 3, 4, 5);
+
+//função anonima (retira o "function" e coloca "=>" depois dos parâmetros)
+//  e se tiver uma linha só no return, tira o "return" e as "{}"
+function adicionarAnonym(...numeros) {
+    let total = numeros.reduce((total, proximo) => total + proximo);
+    console.log(total);
+};
+adicionarAnonym(1, 2, 3, 4);
+
+
+
